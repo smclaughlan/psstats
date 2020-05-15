a084f3b297c1fc6ce47ed47f77b9aa4715d6329f
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-05-15 09:23:24 -0700

    Update config.js for heroku

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export const backEndURL = "http://localhost:8080";
+const environment = process.env.NODE_ENV || "development"
+
+export const backEndURL = (environment === "development") ? "http://localhost:8080" : "https://psstatsback.herokuapp.com";
 export const imgURL = "https://census.daybreakgames.com";
