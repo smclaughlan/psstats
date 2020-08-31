7d992fd7384f796dd66fb70d023b0ef0bb9f16bd
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-08-31 16:29:52 -0700

    move env variables to config.js

--- src/auth0provider.js
+++ src/auth0provider.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { domain, clientId } from "./config";
 
 const Auth0ProviderHistory = ({ children }) => {
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
   const history = useHistory();
 
   const onRedirectCallback = (appState) => {
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const environment = process.env.NODE_ENV || "development"
 
+export const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+export const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 export const backEndURL = (environment === "development") ? "http://localhost:8080" : "https://psstatsback.herokuapp.com";
 export const imgURL = "https://census.daybreakgames.com";
