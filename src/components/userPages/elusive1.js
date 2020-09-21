7bab2e01cd189cf2fc2af11e314e5fe3d56d7ad3
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-09-21 16:55:23 -0700

    update elusive1 character page

--- src/components/userPages/elusive1.js
+++ src/components/userPages/elusive1.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Box } from 'grommet';
+import ReactPlayer from 'react-player';
+import { Box, Paragraph } from 'grommet';
 
 const Elusive1Page = () => {
 
   return (
     <Box animation="fadeIn" className="basic" align="center">
-      <p>This only shows up on elusive1's page</p>
+      <ReactPlayer url={"https://www.youtube.com/watch?v=Dg9N-lka1-8"} controls={true} playing volume={0.1} />
+      <p className="paragraph">Elusive1 primarily plays the infiltrator class in close quarters. He uploads gameplay to youtube.</p>
     </Box>
   )
 }
