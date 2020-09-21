515eb744a73a95bd6ba1c0d117438aa5e7fb471b
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2020-09-21 16:14:58 -0700

    add basic info to elusive1 page

--- src/components/userPages/elusive1.js
+++ src/components/userPages/elusive1.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import { Box } from 'grommet';
+import { Box } from 'grommet';
 
 const Elusive1Page = () => {
 
   return (
-    <>
+    <Box animation="fadeIn" className="basic" align="center">
       <p>This only shows up on elusive1's page</p>
-    </>
+    </Box>
   )
 }
 
