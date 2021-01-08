b0c5dc300cae1e89a569400862b2b346f3d84fe6
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-01-08 12:13:23 -0800

    Use nonarrow function for component

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "grommet";
 
-const LoginButton = () => {
+function LoginButton() {
   const { loginWithRedirect } = useAuth0();
   return (
     <Button
