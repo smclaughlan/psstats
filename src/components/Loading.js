310eb3793975ac8ac5ceb8ef1de9b4a5adb62ec0
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-01-08 12:13:07 -0800

    Use nonarrow function for component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box } from 'grommet';
 
-const Loading = () => {
+function Loading() {
   return (
     <Box animation="fadeIn" >
       <img className="loader" width="30" alt="Loading..." src="/images/loading.gif" />
