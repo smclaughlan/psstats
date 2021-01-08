474172744b3a7fe19ee5c603cde2c2c8f029c0ec
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-01-08 12:18:09 -0800

    Use nonarrow function for component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import LogoutButton from './LogoutButton';
 import LoginButton from './LoginButton';
 
-const NavBar = () => {
+function NavBar() {
   let { isAuthenticated } = useAuth0();
 
   return (
