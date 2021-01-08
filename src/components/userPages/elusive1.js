107af89891fdb17db39a6b5b17ff5de7cbbe6e15
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-01-08 12:03:53 -0800

    Use nonarrow function for component

--- src/components/userPages/elusive1.js
+++ src/components/userPages/elusive1.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import { Box } from 'grommet';
 
-const Elusive1Page = () => {
+function Elusive1Page() {
 
   return (
     <Box animation="fadeIn" className="basic" align="center">
