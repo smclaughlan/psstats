a786ef57dee5dba44f11a764d7ea6b99592900c4
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-01-08 12:04:24 -0800

    Use nonarrow function for component

--- src/components/userPages/flashy.js
+++ src/components/userPages/flashy.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import { Box } from 'grommet';
 
-const FlashyPage = () => {
+function FlashyPage() {
 
   return (
     <Box animation="fadeIn" className="basic" align="center">
