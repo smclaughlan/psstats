d78fb63988f7b392546f8c3cd8503827fffa4703
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-02-16 14:57:17 -0800

    Add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, Anchor } from "grommet";
+
+function Footer() {
+
+  return (
+    <Footer background="brand" pad="medium">
+      <Text>Copyright</Text>
+      <Anchor label="About" />
+    </Footer>
+  )
+}
+
+export default Footer;
