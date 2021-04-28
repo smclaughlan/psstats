5d86d9009cd495907ea3ecfb53bd8a1bed407bcc
Sean McLaughlan <sean.s.mclaughlan@gmail.com>
2021-04-28 15:11:33 -0700

    Reformat

--- src/components/CharacterClassAccuracy.js
+++ src/components/CharacterClassAccuracy.js
@@ -1,43 +1,52 @@
-import React from 'react';
-import { Box, DataTable, Meter } from 'grommet';
-import Loading from './Loading';
+import React from "react";
+import { Box, DataTable, Meter } from "grommet";
+import Loading from "./Loading";
 
 function CharacterClassAccuracy({ stats }) {
-
-  const getChartData = stats => {
+  const getChartData = (stats) => {
     let chartData = [];
     stats.forEach((stat) => {
       if (stat.stat_name === "hit_count") {
         let profileNum = stat.profile_id;
-        stats.forEach(fireStat => {
-          if (fireStat.stat_name === "fire_count" && fireStat.profile_id === profileNum) {
+        stats.forEach((fireStat) => {
+          if (
+            fireStat.stat_name === "fire_count" &&
+            fireStat.profile_id === profileNum
+          ) {
             chartData.push({
               name: stat.class.name.en,
-              percent: Number.parseInt(stat.value_forever) / Number.parseInt(fireStat.value_forever) * 100,
-              percentStr: `${Math.floor((Number.parseInt(stat.value_forever) / Number.parseInt(fireStat.value_forever) * 100))}%`,
+              percent:
+                (Number.parseInt(stat.value_forever) /
+                  Number.parseInt(fireStat.value_forever)) *
+                100,
+              percentStr: `${Math.floor(
+                (Number.parseInt(stat.value_forever) /
+                  Number.parseInt(fireStat.value_forever)) *
+                  100
+              )}%`,
             });
           }
-        })
+        });
       }
-    })
+    });
     return chartData;
-  }
+  };
 
-  return (stats ?
+  return stats ? (
     <Box>
       <h3>Accuracy per class</h3>
       <DataTable
         columns={[
           {
-            property: 'name',
+            property: "name",
             header: <h3>Class</h3>,
             primary: true,
           },
           {
-            property: 'percent',
+            property: "percent",
             header: <h3>Percentage of hits landed</h3>,
-            render: datum => (
-              <Box pad={{ vertical: 'xsmall' }}>
+            render: (datum) => (
+              <Box pad={{ vertical: "xsmall" }}>
                 <Meter
                   values={[{ value: datum.percent }]}
                   thickness="medium"
@@ -48,16 +57,16 @@ function CharacterClassAccuracy({ stats }) {
             ),
           },
           {
-            property: 'percentStr',
-            header: '',
+            property: "percentStr",
+            header: "",
           },
         ]}
         data={getChartData(stats)}
       />
     </Box>
-    :
+  ) : (
     <Loading />
-  )
+  );
 }
 
 export default CharacterClassAccuracy;
